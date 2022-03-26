2caf9fa0317488769acc26d56b6d4b3fec3dbd5a
Md. Mehraj Hossain <mdmehrajshakil98@gmail.com>
2022-03-26 21:47:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const arr = [
+       {
+         Name: 'Muhammad Mehraj Hossain',
+         Roll: 'ASH1801002M',
+         Dept: 'CSTE',
+         Session: '2017-18'
+       },
+       {
+        Name: 'Muhammad Mahadi Hasan Sad',
+        Roll: 'ASH1801003M',
+        Dept: 'CSTE',
+        Session: '2017-18'
+      },
+      {
+        Name: 'Muhammad Mirz',
+        Roll: 'ASH1801004M',
+        Dept: 'CSTE',
+        Session: '2017-18'
+      },
+      {
+        Name: 'Muhammad',
+        Roll: 'ASH1801005M',
+        Dept: 'CSTE',
+        Session: '2017-18'
+      }
+  ];
+  return ( 
+
+          <table className='table table-hover table-dark table-bordered'>
+            <thead className=''> 
+              <tr> 
+                <th> Name </th>
+                <th> Roll </th>
+                <th> Department</th>
+                <th> Session </th>
+              </tr>
+            </thead>
+            <tbody > 
+              {arr.map((student  ) => 
+              <tr key={student.Roll}> 
+                <td>
+                  {student.Name}
+                </td>
+                <td>
+                  {student.Roll}
+                </td>
+                <td>
+                  {student.Dept}
+                </td>
+                <td>
+                  {student.Session}
+                </td>
+              </tr>
+              )}
+            </tbody>
+          </table>
+   );
 }
 
 export default App;
